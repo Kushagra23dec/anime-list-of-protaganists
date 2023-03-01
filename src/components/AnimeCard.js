07d4e292eac851e39253cc4c3c564afc0030c0c8
kushagra23dec <kushagra23dec@gmail.com>
2023-03-01 13:23:40 +0530

    dc

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -18,13 +18,13 @@ const AnimeCard = ({
     <div className=" m-4 flex flex-wrap  text-gray-300 text-lg font-semibold">
       <img className="w-[255px] mr-5" src={image} />
       <div>
-        <h1> <span className="font-bold text-gray-400">Title:</span> {animetitle}</h1>
-        <h1>Synonyms: {Synonyms}</h1>
-        <h1>Episodes: {Episodes}</h1>
-        <h1>Genres: {Genres}</h1>
-        <h1>Theme: {Theme}</h1>
-        <h1>Demographic: {Demographic}</h1>
-        <h1>Score: {Score}</h1>
+      <h1><span className="font-bold text-gray-400">Title: </span> {animetitle}</h1>
+      <h1><span className="font-bold text-gray-400">Synonyms: </span>{Synonyms}</h1>
+      <h1><span className="font-bold text-gray-400">Episodes: </span>{Episodes}</h1>
+      <h1><span className="font-bold text-gray-400">Genres: </span>{Genres}</h1>
+      <h1><span className="font-bold text-gray-400">Theme: </span>{Theme}</h1>
+      <h1><span className="font-bold text-gray-400">Demographic: </span>{Demographic}</h1>
+      <h1><span className="font-bold text-gray-400">Score: </span>{Score}</h1>
         <button
           className="px-3 py-2 rounded-lg  bg-pink-900 hover:bg-pink-800"
           onClick={() => {
--- src/components/Characters.js
+++ src/components/Characters.js
@@ -14,19 +14,19 @@ const Characters = ({
   description,
 }) => {
   return (
-    <div className="flex mt-4">
+    <div className="flex flex-wrap mt-4">
       <img className="w-[255px] h-[350px] mr-4" src={image} />
       <div>
-        <h1>name: {name}</h1>
-        <h1>Birthplace: {Birthplace}</h1>
-        <h1>Date of Birth: {DateofBirth}</h1>
-        <h1>Age: {Age}</h1>
-        <h1>Family: {Family}</h1>
-        <h1>Species: {Species}</h1>
-        <h1>Occupation: {Occupation}</h1>
-        <h1>Abilities: {Abilities}</h1>
-        <h1>Weapon: {Weapon}</h1>
-        <h1>Description: {description}</h1>
+        <h1><span className="font-bold text-gray-400">Name:</span> {name}</h1>
+        <h1><span className="font-bold text-gray-400">Birthplace:</span> {Birthplace}</h1>
+        <h1><span className="font-bold text-gray-400">Date of Birth: </span>{DateofBirth}</h1>
+        <h1><span className="font-bold text-gray-400">Age: </span> {Age}</h1>
+        <h1><span className="font-bold text-gray-400">Family: </span> {Family}</h1>
+        <h1><span className="font-bold text-gray-400">Species: </span> {Species}</h1>
+        <h1><span className="font-bold text-gray-400">Occupation: </span>{Occupation}</h1>
+        <h1><span className="font-bold text-gray-400">Abilities: </span>{Abilities}</h1>
+        <h1><span className="font-bold text-gray-400">Weapon: </span>{Weapon}</h1>
+        <h1><span className="font-bold text-gray-400">Description:  </span> {description}</h1>
       </div>
     </div>
   );
